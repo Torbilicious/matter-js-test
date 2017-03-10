90c3000d9b17d48f313805d7ef0e8366347ef36c
Torben Binder <torben.binder@googlemail.com>
2017-03-10 20:46:49 +0100

    Experiment with a few things

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ var Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
     Bodies = Matter.Bodies,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
     engine,
     render;
 
 main = function () {
-
 
     // create an engine
     engine = Engine.create();
@@ -15,16 +16,32 @@ main = function () {
     // create a renderer
     render = Render.create({
         element: document.getElementById("renderer"),
-        engine: engine
+        engine: engine,
+        options: {
+            width: Math.min(document.documentElement.clientWidth, 800),
+            height: Math.min(document.documentElement.clientHeight, 600)
+        }
     });
 
     // create two boxes and a ground
-    var boxA = Bodies.rectangle(400, 200, 80, 80);
-    var boxB = Bodies.rectangle(450, 50, 80, 80);
-    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    var boxA = Bodies.rectangle(400, 200, 50, 100);
+    var circle = Bodies.circle(200, 200, 50, { restitution: 0.9 });
+
+    setGravity(0, 0);
+
+    var mouseSettings = addMouseConstraints();
+    World.add(engine.world, mouseSettings.mouseConstraint);
+    render.mouse = mouseSettings.mouse;
 
     // add all of the bodies to the world
-    World.add(engine.world, [boxA, boxB, ground]);
+    World.add(engine.world, [
+        boxA,
+        circle,
+        Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+        Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+        Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+        Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+    ]);
     // run the engine
     Engine.run(engine);
     // run the renderer
@@ -35,4 +52,30 @@ createNewBox = function () {
 
     var box = Bodies.rectangle(450, 50, 80, 80);
     World.add(engine.world, [box]);
+};
+
+setGravity = function(xGrav, yGrav) {
+
+    var gravity = engine.world.gravity;
+    gravity.x = xGrav;
+    gravity.y = yGrav;
+};
+
+addMouseConstraints = function () {
+
+    var mouse = Mouse.create(render.canvas),
+        mouseConstraint = MouseConstraint.create(engine, {
+            mouse: mouse,
+            constraint: {
+                stiffness: 0.2,
+                render: {
+                    visible: false
+                }
+            }
+        });
+
+    return {
+        mouse: mouse,
+        mouseConstraint: mouseConstraint
+    };
 };
